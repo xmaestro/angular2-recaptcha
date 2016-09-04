4625eae53fb6cab9fa1e13111042e751c80b6b79
xmaestro <alexeyfedorov101@gmail.com>
2016-09-04 18:19:16 +0500

    Updated to RC6

--- angular2-recaptcha.ts
+++ angular2-recaptcha.ts
@@ -1,1 +1,7 @@
-export * from './components/captcha';
+import { NgModule } from '@angular/core';
+import { ReCaptchaComponent } from "./lib/captcha";
+@NgModule({
+    declarations: [ReCaptchaComponent],
+    exports: [ReCaptchaComponent]
+})
+export class ReCaptchaModule {}
