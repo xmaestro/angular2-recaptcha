f292c9e61152dce6149a8701b5f0f8ae1cfca509
Achim Hasenmueller <achim@autorouter.eu>
2016-09-07 11:29:50 +0200

    Commit compiled ES5 code

--- angular2-recaptcha.ts
+++ angular2-recaptcha.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ReCaptchaComponent } from "./lib/captcha";
+
 @NgModule({
     declarations: [ReCaptchaComponent],
     exports: [ReCaptchaComponent]
